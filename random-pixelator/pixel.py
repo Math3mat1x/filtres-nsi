7e2535de31d7fb0e9ea130f6d81b5df6c370b31b
Maxime Corne <maxime.corne@protonmail.com>
2020-11-12 18:02:42 +0100

    The image is now pixelated correctly

--- random-pixelator/pixel.py
+++ random-pixelator/pixel.py
@@ -4,25 +4,50 @@ im = Image.open("image.png")
 
 w, h = im.size
 
-block_size = 3
+block_size = 3 # TODO: other values don't work...
 
-blocks = list()
-x_current, y_current = -1, -1
+def average_block_color(start_x, start_y):
+    blocks = list()
+    for y in range(block_size):
+        if y > 0:
+            blocks.append(block)
+        block = list()
+        for x in range(block_size):
+            if w >= start_x + x + 1 or h >= start_y + y + 1:
+                block.append(im.getpixel((start_x+x-1, start_y+y-1)))
+            else:
+                block.append((0, 0, 0))
 
-blocks.append([list() for i in range(block_size)])
+    color = [[], [], []]
+    color_depth = len(blocks[0][0])
 
-current_block = list()
+    for block in blocks:
+        for i in range(block_size):
+            for j in range(color_depth):
+                color[j].append(block[i][j])
 
-for y in range(block_size):
-    y_current += 1
-    x_block = list()
-    if y >= 0:
-        current_block.append(x_block)
-    for x in range(block_size):
-        x_current += 1
-        if w >= x_current + 1 or h >= y_current + 1:
-            x_block.append(im.getpixel((x_current, y_current)))
-        else:
-            x_block.append((0,0,0))
+    color = tuple([round(sum(i)/len(i)) for i in color])
 
-print(current_block[0])
+    return color
+
+
+
+def draw(x, y, color):
+    for i in range(block_size):
+        for j in range(block_size):
+            if i + x <= w and j + y <= h:
+                try:
+                    im.putpixel((i+x-1, j+y-1), color)
+                except:
+                    print(i+x, j+y)
+                    input()
+
+# main loop
+for x in range(-1, w, block_size):
+    for y in range(-1, h, block_size):
+        # print(x,y)
+        color = average_block_color(x, y)
+        draw(x, y, color)
+
+print(im.size)
+im.save("test.png", "PNG")
